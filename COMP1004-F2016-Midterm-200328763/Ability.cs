31b73d37e753498971ae7ff051cbafff66e1c091
Christopher Shannon <200328763@student.georgianc.on.ca>
2016-10-20 16:06:28 -0400

    Add project files.

---
+++ COMP1004-F2016-Midterm-200328763/Ability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP1004_F2016_Midterm_200328763
+{
+    public enum Ability
+    {
+        Strength,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Charisma
+    }
+}
